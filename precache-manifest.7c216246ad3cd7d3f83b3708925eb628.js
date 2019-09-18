0701d9b76dd3f1406d6642fb15c7859fee05fb8c
Max Jung <jung.moonki94@gmail.com>
2019-09-17 23:24:55 -0700

    Updates

---
+++ precache-manifest.7c216246ad3cd7d3f83b3708925eb628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf651ce20d0954b75c08d13b79becbef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "86741fb7865d603c6ad1",
+    "url": "/monsters-rolodex/static/css/main.6364a27f.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "86741fb7865d603c6ad1",
+    "url": "/monsters-rolodex/static/js/main.94989dc6.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
